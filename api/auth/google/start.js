cc7b36078d93711b5f9aa78c7f964cec25e972cd
greggiek <greg@bargainmoulding.com>
2026-08-06 07:49:01 -0400

    Use shared library from api/auth/google/start.js

--- api/auth/google/start.js
+++ api/auth/google/start.js
@@ -1,5 +1,5 @@
-const { requireManager } = require("../../_auth");
-const { env, signState } = require("../../_google");
+const { requireManager } = require("../../../lib/auth");
+const { env, signState } = require("../../../lib/google");
 
 module.exports = async function handler(req, res) {
   if (req.method !== "GET") return res.status(405).json({ error: "Method not allowed." });
